c372fff7c9936a0e51d5cfc141136776927745f5
Leyton McKinney <leyton.mckinney@gmail.com>
2025-10-29 12:36:02 -0500

    init: add hello window opengl program

---
+++ build/CMakeFiles/obj_viewer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obj_viewer.dir/src/glad.c.o"
+  "CMakeFiles/obj_viewer.dir/src/glad.c.o.d"
+  "CMakeFiles/obj_viewer.dir/src/main.cpp.o"
+  "CMakeFiles/obj_viewer.dir/src/main.cpp.o.d"
+  "obj_viewer"
+  "obj_viewer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/obj_viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
